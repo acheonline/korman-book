236a73e9ebdaf5b7cc6b33872d827ff76a465abc
Чернявский Антон Геннадьевич (4058290) <agchernyavskii@vtb.ru>
2022-11-23 06:37:47 +0300

    add 70 leetcode - climbing stairs (and complexity description)

--- leetcode/climbing_steps_70.go
+++ leetcode/climbing_steps_70.go
@@ -7,15 +7,19 @@ func main() {
 }
 
 func climbStairs(n int) int {
-	if n < 3 {
+	if n < 3 { // O(0)
 		return n
 	}
 
-	dp := make([]int, n+1)
-	dp[1] = 1
-	dp[2] = 2
-	for i := 3; i <= n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
+	dp := make([]int, n+1)    // O(0)
+	dp[1] = 1                 // O(0)
+	dp[2] = 2                 // O(0)
+	for i := 3; i <= n; i++ { // O(n)
+		dp[i] = dp[i-1] + dp[i-2] //O(0)
 	}
-	return dp[n]
+	return dp[n] //O(0)
 }
+
+// whole time spent is O(0) + O(0) + O(0) + O(0) + N * (O(0)) + O(0)
+// simple record is - c*O(0) + O(n)
+// more simple - O(n)
