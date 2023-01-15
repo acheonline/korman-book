c642043858d66de25f8aca60f24a5d41e97eb0a0
Чернявский Антон Геннадьевич (4058290) <agchernyavskii@vtb.ru>
2023-01-15 08:33:37 +0300

    add 171 leetcode - excel_sheet_column_number

---
+++ leetcode/excel_sheet_column_number_171.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+//Constraints:
+
+//1 <= columnTitle.length <= 7
+//columnTitle consists only of uppercase English letters.
+//columnTitle is in the range ["A", "FXSHRXW"].
+
+func main() {
+	//	fmt.Println(titleToNumber("A"), "expected 1")
+	//	fmt.Println(titleToNumber("AB"), "expected 28")
+	fmt.Println(titleToNumber("ZY"), ", expected 701")
+
+}
+
+func titleToNumber(columnTitle string) int {
+	st := []rune(columnTitle)
+	res := 0
+	for index := len(st) - 1; index >= 0; index-- {
+		pow := math.Pow(26, float64(len(st)-1-index))
+		res += int(st[index]) % 64 * int(pow)
+	}
+	return res
+}
